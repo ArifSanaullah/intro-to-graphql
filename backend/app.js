f9949599dc97ad8b8422cb17f77b7377cd1d0970
Arif Sanaullah <ak7896023@gmail.com>
2020-12-17 16:39:32 +0500

    feat: Server setup done

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { json } = require("body-parser");
+
+const app = express();
+
+app.use(json());
+
+app.listen(
+  (3000,
+  () => {
+    console.log("Listening on port 3000");
+  })
+);
