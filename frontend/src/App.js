f2a5935257bd794d918509061a27f259c82fe365
Arif Sanaullah <ak7896023@gmail.com>
2020-12-22 16:11:14 +0500

    feat: Done with structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Events from './pages/Events';
 import Bookings from './pages/Bookings';
+import MainNavigation from './components/navigation/MainNavigation';
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Switch>
-					<Redirect from="/" to="/auth" exact />
-					<Route path="/auth" component={Auth} />
-					<Route path="/events" component={Events} />
-					<Route path="/bookings" component={Bookings} />
-				</Switch>
+				<>
+					<MainNavigation />
+					<main className="main-content">
+						<Switch>
+							<Redirect from="/" to="/auth" exact />
+							<Route path="/auth" component={Auth} />
+							<Route path="/events" component={Events} />
+							<Route path="/bookings" component={Bookings} />
+						</Switch>
+					</main>
+				</>
 			</BrowserRouter>
 		</div>
 	);
